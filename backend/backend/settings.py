1476ee8fabcc796154e35407ce6e62668301feb2
Quang Minh <157796411+minhminh12315@users.noreply.github.com>
2025-05-19 12:19:08 +0700

    thay sqlite bang mysql

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,8 +80,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shop',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # hoặc IP của MySQL server
+        'PORT': '3306',        # cổng mặc định của MySQL
     }
 }
 
