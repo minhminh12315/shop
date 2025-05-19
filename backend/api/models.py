9643d73364f440f01c96306689c5fd81ce74e474
Quang Minh <157796411+minhminh12315@users.noreply.github.com>
2025-05-20 01:16:04 +0700

    update db

--- backend/api/models.py
+++ backend/api/models.py
@@ -24,9 +24,15 @@ class User(models.Model):
     
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
     order_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Order {self.id} by {self.user.username}"
+
+class OrderDetail(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return f"Order Detail {self.id} for Order {self.order.id}"
