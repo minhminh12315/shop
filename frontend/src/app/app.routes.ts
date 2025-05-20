26f0b4e291a42e74af5be927af579c3952793d0a
Lê Đức Ân <162079456+anduc0104@users.noreply.github.com>
2025-05-20 18:55:46 +0700

    An: giao dien login, signup

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,12 +5,16 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { NgModule } from '@angular/core';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
     // {path: '', component: AppComponent},
   { path: 'products', component: ProductListComponent },
   { path: '', component: HomePageComponent },
   { path: 'product-detail/:id', component: ProductDetailComponent}, // chuyển hướng về trang chủ nếu không tìm thấy route
+  { path: 'login', component: LoginComponent}, // chuyển hướng về trang chủ nếu không tìm thấy route
+  { path: 'register', component: RegisterComponent}, // chuyển hướng về trang chủ nếu không tìm thấy route
   // có thể thêm các route khác sau
 ];
 
