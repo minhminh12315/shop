e1840e2b063062e410325f8b5c136c321ac2d8a8
Lê Đức Ân <162079456+anduc0104@users.noreply.github.com>
2025-05-21 14:30:57 +0700

    AN: merge main, cart

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,13 +5,16 @@ import { HomePageComponent } from './components/home-page/home-page.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { CartProductComponent } from './components/cart-product/cart-product.component';
 
 export const routes: Routes = [
   { path: 'products', component: ProductListComponent },
   { path: '', component: HomePageComponent },
-  { path: 'product-detail/:id', component: ProductDetailComponent},
-  { path: 'login', component: LoginComponent},
-  { path: 'register', component: RegisterComponent},
+  { path: 'product-detail/:id', component: ProductDetailComponent}, // chuyển hướng về trang chủ nếu không tìm thấy route
+  { path: 'login', component: LoginComponent}, // chuyển hướng về trang chủ nếu không tìm thấy route
+  { path: 'register', component: RegisterComponent}, // chuyển hướng về trang chủ nếu không tìm thấy route
+  { path: 'cart', component: CartProductComponent}, // chuyển hướng về trang chủ nếu không tìm thấy route
+  // có thể thêm các route khác sau
 ];
 
 @NgModule({
